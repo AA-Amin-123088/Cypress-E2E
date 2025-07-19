e51a160c5ea64e486bfc1d54e5534feffe274b6c
Alamin2017 <alamincse12@gmail.com>
2025-07-19 13:13:19 +0600

    updated the  file where is needed

--- cypress/e2e/Test_case_10.cy.js
+++ cypress/e2e/Test_case_10.cy.js
@@ -10,19 +10,17 @@ describe('Cypress Session Example', () => {
       cy.get('[data-test="password"]').type('secret_sauce');
       cy.get('[data-test="login-button"]').click();
       cy.url().should('include', 'inventory.html');
-    },{
+    },
+    {
       validate: () => {
-        // Validate if session is still active; if not, it will be re-created
         cy.url().should('include', 'inventory.html');
       }
     });
-    
   });
 
   it('Test Case 1: Verify Features Items', () => {
     cy.xpath("//div[normalize-space()='Sauce Labs Backpack']").should('exist');
   });
-
   it('Test Case 2: Scroll to Bottom', () => {
     cy.xpath("//div[normalize-space()='Sauce Labs Backpack']").should('be.visible').click();
     cy.get('[data-test="add-to-cart"]').click();
